9875028ec34d7a611c4a104b099feceee45e496a
kirbycampbell <jkirbycampbell@gmail.com>
2019-05-17 14:46:07 -0700

    Runnin Smoothly

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import useInterval from "./useInterval";
 
 function App() {
   const [transition, setTransition] = useState(false);
+  const [boxMove, setBoxMove] = useState(false);
+  useEffect(() => {
+    setBoxMove(!boxMove);
+  }, [transition]);
+  useInterval(() => {
+    setTransition(!transition);
+  }, 4000);
 
   return (
     <div className="App">
       <div className={"header " + (transition ? "finish" : "start")}>Hello</div>
       <div className="content-box">
-        <button onClick={() => setTransition(!transition)}>
-          Click to set Transition as opposite
-        </button>
+        <div className="outerBox ">
+          <div className={"innerBox " + (boxMove ? "Lbox-start" : "Lbox-fin")}>
+            Left Box
+          </div>
+          <div className={"innerBox " + (boxMove ? "Rbox-start" : "Rbox-fin")}>
+            Right Box
+          </div>
+        </div>
       </div>
-      <div className="footer">
-        <div className="innerFooter">Footer</div>
+      <div className={"footer " + (transition ? "foot-fin" : "foot-start")}>
+        Footer
       </div>
     </div>
   );
